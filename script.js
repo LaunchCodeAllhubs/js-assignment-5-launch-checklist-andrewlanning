297c1f4eb149017e0d44b80f7c9e6024b962c862
Andrew Lanning <116489166+andrewlanning@users.noreply.github.com>
2023-08-01 00:47:42 -0500

    Update script.js
    Test Commit, Issues with npm test locally.

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-// Write your JavaScript code here!
-
 window.addEventListener("load", function() {
+    let form = document.querySelector("[data-testid='testForm']");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Requirement 1
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        const pilot = document.getElementById("pilotName").value;
+        const copilot = document.querySelector("input[name='copilotName']").value;
+        const fuelLevel = document.querySelector("input[name='fuelLevel']").value;
+        const cargoLevel = document.querySelector("input[name='cargoMass']").value;
+
+        if (!pilot || !copilot || !fuelLevel || !cargoLevel) { // Add an alert to notify the user that all fields are required.
+            window.alert("All fields are required!");
+            return;
+        }
+
+        if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+            window.alert("Make sure to enter valid information for each field!")
+            return;
+        }
+
+        formSubmission(document, pilot, copilot, fuelLevel, cargoLevel);
    })
-   
+
+    let listedPlanets;
+    let listedPlanetsResponse = myFetch();
+
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+
+   }).then(function () {
+        console.log(listedPlanets);
+        const selectedPlanet = pickPlanet(listedPlanets);
+        addDestinationInfo(
+            document,
+            selectedPlanet.name,
+            selectedPlanet.diameter,
+            selectedPlanet.star,
+            selectedPlanet.distance,
+            selectedPlanet.moons,
+            selectedPlanet.image
+        )
+   })
 });
