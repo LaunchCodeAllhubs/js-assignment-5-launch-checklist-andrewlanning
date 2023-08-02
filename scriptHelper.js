c1b2282b6f266c487006f3453f194147d4aff9cf
Andrew Lanning <116489166+andrewlanning@users.noreply.github.com>
2023-08-01 20:16:03 -0500

    Update scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -36,46 +36,33 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     const launchStatus = document.getElementById("launchStatus");
     const cargoStatus = document.getElementById("cargoStatus");
 
-    pilotStatus.textContent = `Pilot ${pilot} is ready for launch`
-    copilotStatus.textContent= `Co-pilot ${copilot} is ready for launch`
-
     if (!pilot || !copilot || !fuelLevel || !cargoLevel) { // Add an alert to notify the user that all fields are required.
         alert("All fields are required!");
-        return;
-    }
-
-    if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
-        alert("Make sure to enter valid information for each field!")
-        return;
-    }
-
-    if (Number(fuelLevel) < 10000 && Number(cargoLevel) > 10000) {
-        list.style.visibility = "visible"; // change faultyItems to visible
-        //faultyItems.style.visibility = "visible"; // change faultyItems to visible
-        fuelStatus.textContent = "Not enough fuel for the journey."; // with an updated status stating that there is not enough fuel for the journey.
-        cargoStatus.textContent = "Cargo mass too heavy for launch"; // with an updated cargo status stating that there is too much mass for the shuttle to take off.
-        launchStatus.textContent = "Shuttle Not Ready for Launch"; // The text of the h2 element, launchStatus, should also change to "Shuttle not ready for launch"
-        launchStatus.style.color = "rgb(199, 37, 78)"; // The color should change to a particular shade of red, #37254E.
-    } else if (Number(fuelLevel) < 10000) { // If the user submits a fuel level that is too low (less than 10,000 liters)
-        list.style.visibility = "visible"; // change faultyItems to visible
-        //faultyItems.style.visibility = "visible"; // change faultyItems to visible
-        fuelStatus.textContent = "Not enough fuel for the journey."; // with an updated status stating that there is not enough fuel for the journey.
-        launchStatus.textContent = "Shuttle Not Ready for Launch"; // The text of the h2 element, launchStatus, should also change to "Shuttle not ready for launch"
-        launchStatus.style.color = "red"; // The color should change to red
-    } else if (Number(cargoLevel) > 10000) { // If the user submits a cargo mass that is too large (more than 10,000 kilograms)
-        list.style.visibility = "visible"; // change the list to visible
-        //faultyItems.style.visibility = "visible"; // change faultyItems to visible
-        cargoStatus.textContent = "Cargo mass too heavy for launch"; // with an updated cargo status stating that there is too much mass for the shuttle to take off.
-        launchStatus.textContent = "Shuttle Not Ready for Launch"; // The text of launchStatus should also change to "Shuttle not ready for launch"
-        launchStatus.style.color = "rgb(199, 37, 78)"; // The color should change to a particular shade of red, #37254E.
+    } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+        alert("Make sure to enter valid information for each field!");
     } else {
-        list.style.visibility = "visible";
-        //faultyItems.style.visibility = "visible"; // change faultyItems to visible
-        launchStatus.textContent = "Shuttle is Ready for Launch";
-        launchStatus.style.color = "rgb(65, 159, 106)";
-        cargoStatus.textContent = "Cargo mass low enough for launch";
-        fuelStatus.textContent = "Fuel level high enough for launch";
-    }  
+        pilotStatus.textContent = `Pilot ${pilot} is ready for launch`
+        copilotStatus.textContent= `Co-pilot ${copilot} is ready for launch`
+        list.style.visibility = "visible"; // change faultyItems to visible
+        if (Number(fuelLevel) <10000) {
+            fuelStatus.textContent = "Not enough fuel for the journey."; // with an updated status stating that there is not enough fuel for the journey.
+            launchStatus.textContent = "Shuttle Not Ready for Launch"; // The text of the h2 element, launchStatus, should also change to "Shuttle not ready for launch"
+            launchStatus.style.color = "red"; // The color should change to red
+        }else{
+            fuelStatus.textContent = "Fuel level high enough for launch";
+        }
+        if (Number(cargoLevel) > 10000) {
+            cargoStatus.textContent = "Cargo mass too heavy for launch"; // with an updated cargo status stating that there is too much mass for the shuttle to take off.
+            launchStatus.textContent = "Shuttle Not Ready for Launch"; // The text of launchStatus should also change to "Shuttle not ready for launch"
+            launchStatus.style.color = "rgb(199, 37, 78)"; // The color should change to a particular shade of red, #37254E.
+        }else{
+            cargoStatus.textContent = "Cargo mass low enough for launch";
+        }
+        if (cargoStatus.textContent === "Cargo mass low enough for launch" && fuelStatus.textContent === "Fuel level high enough for launch") {
+            launchStatus.textContent = "Shuttle is Ready for Launch";
+            launchStatus.style.color = "rgb(65, 159, 106)";
+        }
+    }
 }
 
 async function myFetch() {
